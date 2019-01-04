665c46567ba71c9f27fced2ca8027266e97ff413
Paul Kögel <i.paul.koegel@gmail.com>
2019-01-04 22:29:46 +0100

    rollup.config.js: fix JSON structure.

--- rollup.config.js
+++ rollup.config.js
@@ -4,18 +4,18 @@ import babel from 'rollup-plugin-babel';
 const config = {
   input: 'src/juicer_feed.js',
   external: ['react'],
+  plugins: [
+    babel({
+      exclude: "node_modules/**"
+    }),
+    uglify()
+  ],
   output: {
    format: 'umd',
    name: 'JuicerFeed',
    globals: {
      react: 'React'
-   },
-   plugins: [
-     babel({
-       exclude: "node_modules/**"
-     }),
-     uglify()
-   ]
+   }
   }
 };
 
